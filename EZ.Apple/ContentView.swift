22ec022efe4a10229b5c4c6606de1e218287dfc6
youihyonKim <insidesy4@gmail.com>
2023-05-04 10:25:14 +0900

    m test

--- EZ.Apple/ContentView.swift
+++ EZ.Apple/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
             Text("Hello, world!")
             
             Text("Hello puppy")
+            
+            Text("Hello Seez")
         }
         .padding()
     }

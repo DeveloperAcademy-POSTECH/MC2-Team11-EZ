1b24b2c4fe9092b246e9cffe24ecc946bd7001ad
Genie <traum6003@gmail.com>
2023-05-10 15:59:18 +0900

    [RecordView/CHORE] RocordView 초기 세팅

---
+++ EZ.Apple/RecordView/RecordView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordView.swift
+//  EZ.Apple
+//
+//  Created by Park Hyejin on 2023/05/10.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView()
+    }
+}
